68a87629df26bc294944c55f168df992f741cffe
RishavGupta868 <rishavgupta868@gmail.com>
2024-08-21 22:37:50 +0530

    added a functionality where the selectbox is only visible on About Us page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 
-import { Link } from 'react-router-dom'
+
+import { Link, useLocation } from 'react-router-dom'
 
 const Header = ({lang , setLang}) => {
-  
+  const location = useLocation();
+  const isAboutPage = location.pathname === '/about';
   return (
     <div className='text-center bg-orange-400 p-4 '>
       <h1 className='font-extrabold text-xl text-blue-700 p-2'>Meme Page</h1>
       <nav className='flex flex-row p-4 font-semibold text-lg mx-4'>
         <Link to={'/'} className='pr-8'>Home</Link>
-        <Link to={'/about'} className='pr-8' >About Us</Link>
+        <Link to={'/about'}  className='pr-8' >About Us</Link>
         <Link to={'/login'} className='pr-8'>Login</Link>
         <Link to={'/profile'} className='pr-8'>Profile</Link>
         
       </nav>
+      { isAboutPage &&
       <div>
+        
         <label className='px-1 font-semibold'>Select language</label>
         <select className='border-black border p-2' value={lang} onChange={(e)=> setLang(e.target.value)}>
           <option value='en'>English</option>
@@ -22,6 +26,7 @@ const Header = ({lang , setLang}) => {
           <option value='hi'>Hindi</option>
         </select>
       </div>
+}
     </div>
   )
 }
