9509e86bede0da4ed7c57885eeb3a53803a1d069
RishavGupta868 <rishavgupta868@gmail.com>
2024-08-22 22:25:52 +0530

    implemented infinite scroll on meme page

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,36 +1,54 @@
-import React from 'react'
+import React from "react";
 import { useEffect, useState } from "react";
-import MemeCard from './MemeCard';
-import Shimmer from './Shimmer';
+import MemeCard from "./MemeCard";
+import Shimmer from "./Shimmer";
 const Body = () => {
-    const [meme,setMeme]= useState(null)
+  const [meme, setMeme] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const fetchMemes = async ()=>{
+  const fetchMemes = async () => {
+    try {
+      setLoading(true);
+      const data = await fetch("https://meme-api.com/gimme/20");
+      if (!data.ok) {
+        throw new Error(`HTTP Error! Status: ${data.status}`);
+      }
+      const json = await data.json();
+      setMeme((meme) => [...meme, ...json.memes]);
+    } catch (error) {
+      console.error("Failed to fetch memes:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+      document.documentElement.scrollHeight
+    ) {
+      console.log("handle");
+      fetchMemes();
+    }
+  };
 
-    const data = await fetch('https://meme-api.com/gimme/20');
-    const json = await data.json();
-    
-    setMeme(json.memes)
-  }
-  useEffect(()=>{
+  useEffect(() => {
     fetchMemes();
-  },[])
-  if(!meme) return <Shimmer/>
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-
-    <div className='flex flex-row flex-wrap bg-orange-400 pl-9'>
-        {meme && meme.map((element,index)=>{
-            return(
-                <MemeCard key={index} data={element} />
-            )
+    <div className="flex flex-row flex-wrap bg-orange-400 pl-9">
+      {meme &&
+        meme.map((element, index) => {
+          return <MemeCard key={index} data={element} />;
         })}
-     
-        
-            
-        
-    
-    </div>
-  )
-}
 
-export default Body
+      {loading && <Shimmer />}
+    </div>
+  );
+};
+
+export default Body;
